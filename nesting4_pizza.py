d244f44c89ed34febb5f819a168fc2316ccde03b
jingyeong27 <102271635+jingyeong27@users.noreply.github.com>
2022-05-02 11:02:30 +0900

    Update nesting4_pizza.py
    toppings and crust re-enter

--- nesting4_pizza.py
+++ nesting4_pizza.py
@@ -34,20 +34,26 @@ for requested_size in requested_size:
         pizza_crust = input("what kind of crust do you want to have [thick, thin]  ===> ")
         requested_crusts = []
         requested_crusts.append(pizza_crust)
+        
 for requested_crust in requested_crusts:
     if requested_crust in pizza['crust']:
-        toppings = input("what topping do you want to add ===> ")
+        toppings = input("what topping do you want to add [mushrooms/olives/green peppers/pepperoni/pineapple/extra cheese]===> ")
         requested_toppings = []
         requested_toppings.append(toppings)
         for requested_topping in requested_toppings:
             if requested_topping in pizza['toppings']:
-                print("We are adding",requested_topping + ".")
+                print("The composition of the pizza is",requested_size,"size /", requested_crust,"crust /", requested_topping + " pizza!" )
+                print(f"===== Your total pizza price is ${bill}")
             else:
                 print("Sorry,the topping you want to add is out of stock! Could you please choose another topping?")
-        print("\nWe have finished making your pizza")
-                           
-    else:
+                toppings = input("what topping do you want to add [mushrooms/olives/green peppers/pepperoni/pineapple/extra cheese]===> ")
+    else :
         print("Sorry,the crust you want to add is out of stock! Could you please choose another crust?")
+        requested_crusts = []
+        pizza_crust = input("what kind of crust do you want to have [thick, thin]  ===> ")
+        requested_toppings = []
+        toppings = input("what topping do you want to add [mushrooms/olives/green peppers/pepperoni/pineapple/extra cheese]===> ")
+        print("The composition of the pizza is " + pizza_crust,toppings + " pizza ")
         
 order_way = input("Do you want packaging or delivery? ===> ")
 if order_way == "packaging" :
